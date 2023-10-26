be08e418befaab1a5c8d447e0a84774158a95b2d
oddities-o7 <akwilson132@gmail.com>
2023-10-25 21:53:00 -0400

    Update EncoderJorge.py

--- EncoderJorge.py
+++ EncoderJorge.py
@@ -1,10 +1,15 @@
 class PasswordEncoder:
+
     def encode(self, password):
         encoded_password = ''.join(str((int(digit) + 3) % 10) for digit in password)
         return encoded_password
 
     def decode(self, encoded_password):
-        decoded_password = ''.join(str((int(digit) - 3) % 10) for digit in encoded_password)
+        decoded_password = []
+        for j in encoded_password:
+            i = int(j)
+            i -= 3
+            decoded_password.append(i)
         return decoded_password
 
 
@@ -23,12 +28,11 @@ def main():
         if option == '1':
             password = input("Please enter your password to encode: ")
             encoded_password = password_encoder.encode(password)
-            print(f"Your password has been encoded and stored: {encoded_password}\n")
+            print(f"Your password has been encoded and stored!")
 
         elif option == '2':
-            encoded_password = input("Please enter the encoded password to decode: ")
-            decoded_password = password_encoder.decode(encoded_password)
-            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}\n")
+            decoded_password = ''.join(map(str, password_encoder.decode(encoded_password)))
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
 
         elif option == '3':
             break
